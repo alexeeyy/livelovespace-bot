3ec01c65bd946fe5b57517daa7e2e89c31040e33
alexeeyy <alexeyyakuba@gmail.com>
2022-09-23 15:05:27 +0300

    livelovespace

---
+++ const.js
@@ -0,0 +1,21 @@
+const commands = `
+/start - Перезапустити бот
+/help - Допомога
+`;
+
+const textWelcome = `<b>Namaste! LiveLoveSpace бот вітає Вас!</b>`;
+
+const textYoga = `Йога з Анною Паніною 18:30 - 19:30
+Вартість 200грн`;
+
+const textZen = `Дзадзен з Амєтом 18:30 - 19:30
+Вартість 150грн`;
+
+const textQigong = `Цигун із Максом 18:30 - 19:30
+Вартість 150грн`;
+
+module.exports.commands = commands;
+module.exports.textWelcome = textWelcome;
+module.exports.textYoga = textYoga;
+module.exports.textZen = textZen;
+module.exports.textQigong = textQigong;
